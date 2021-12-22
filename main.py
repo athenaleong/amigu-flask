1364b9930033d5fb8aa2c10060a8047215cb46f2
Athena Leong <athenaleong619@gmail.com>
2021-12-22 16:21:04 +0800

    Added all Quesitons

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 from flask import Flask, json, send_from_directory, jsonify, request
 from flask_cors import CORS
+from flask_caching import Cache
 import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
 import random 
 
+
 load_dotenv()
+
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
 app = Flask(__name__)
 CORS(app)
-
+app.config.from_mapping(config)
+cache = Cache(app)
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 
 @app.route("/newQuestions", methods=['POST'])
-def newQuestion():
+def newQuestions():
     #TODO: pull new qn --> axios on frontend --> save to local storage
+    #TODO: make this algo better lol it is so inefficient rn
     numQ = request.json.get('numQ')
     oldQ = request.json.get('oldQ')
     data = supabase.table('question').select('*').execute()['data']
@@ -24,6 +34,24 @@ def newQuestion():
     newQ = random.sample(data, numQ)
     data = jsonify({'newQ': newQ})
     return data
+
+@app.route("/allQuestions", methods=["GET"])
+# @cache.cached(timeout=3600)
+#PROGRAMMER NOTE: select from supabase --> create json file --> set up cache --> check eveyrthing in page works --> move on to pet
+def allQuestions():
+    data = supabase.table('category').select('name').execute()['data']
+    category = [d['name'] for d in data]
+    payload = []
+    for c in category:
+        questions = supabase.table('question').select('content', 'id').eq('category', c).execute()['data']
+        payload.append({'category': c, 'questions': questions})
+    data = jsonify({'payload': payload})
+    return data
+
+
+
+    return 'success', 200
+
 
 @app.route("/updateTable", methods=['POST'])
 def updateTable():
@@ -41,3 +69,4 @@ def updateTable():
     else:
         return 'database error', 500
     return 'success', 200
+
